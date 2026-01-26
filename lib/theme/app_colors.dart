ece83ba154868074f32949b3c6627a0d5a66a734
Penpicha Priwan <Penpicha.pri@gmail.com>
2026-01-26 17:41:51 +0700

    add color

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -10,4 +10,33 @@ class AppColors {
 
   static const Color hint = Color(0xFFBDBDBD);
   static const Color border = Color(0xFFE0E0E0);
+
+  static const Color backgroundPage = Color(0xFFFFF5E6);
+  static const Color backgroundLight = Color(0xFFFCECDD);
+  static const Color backgroundDark = Color(0xFFFFE0B2);
+
+  // 🟠 Primary
+  static const Color orangone = Color(0xFFFFA726);
+  static const Color orangtwo = Color(0xFFFFCC80);
+  static const Color orangtree = Color(0xFFFF9800);
+  static const Color orangfour = Color(0xFFFEA82F);
+
+  // 📝 GreyText
+  static const Color textGrey = Color(0xFF2D2D2D);
+  static const Color textGreyTwo = Color(0xFF666666);
+  static const Color textHint = Color(0xFF9E9E9E);
+
+
+  // ⚪ UI
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFFBDBDBD);
+  static const Color darkGrey = Color(0xFF737373);
+  static const Color lightGrey = Color(0xFFE0E0E0);
+
+  // BlackText
+    static const Color black = Color(0xFF000000);
+    static const Color textBlack = Color(0xDD0A0A0A);
+
+  //Page Indicator
+  static const Color pageIndicatorActive = Color(0xFFFF6701);
 }
